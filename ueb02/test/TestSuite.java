e25af14006af643d31bcff007466c3925867aaa1
Lars Sander <Lars.Sander@protonmail.com>
2020-05-29 07:22:01 +0200

    init

--- ueb02/test/TestSuite.java
+++ ueb02/test/TestSuite.java
@@ -9,7 +9,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         // Testklassen hier ergänzen
-        collatz_test.class })
+        collatz_test.class, vignere_test.class })
 
 public class TestSuite {
 
