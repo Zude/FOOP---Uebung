4185fe0d27004f561ca50ed45d230f3cf5d46e1c
Alexander Löffler <alex.93.loeffler@me.com>
2020-06-01 12:00:56 +0200

    Simple SortByGender Test added

--- ueb02/test/GroupPersonsByGenderTest.java
+++ ueb02/test/GroupPersonsByGenderTest.java
@@ -1,25 +1,47 @@
 import static org.junit.Assert.assertEquals;
 
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import org.junit.Test;
 
+import streamops.Person;
+import streamops.Person.Gender;
 import streamops.StreamOperations;
 
 public class GroupPersonsByGenderTest {
 
     @Test
-    public void test() {
+    public void simpleGroupByGenderTest() {
 
         Stream<String> inputStream = Stream.of("hallo", "a", "", "-.,");
 
-        Stream<Character> resultStream = StreamOperations.stringsToChars(inputStream);
-        ;
+        Person person1 = new Person(Gender.MALE, "Alex", "Löffler", 21337, 10);
+        Person person2 = new Person(Gender.MALE, "Lars", "Sander", 21337, 10);
 
-        Stream<Character> expectedStream = Stream.of('h', 'a', 'l', 'l', 'o', 'a', '-', '.', ',');
+        Set<Integer> zipcodesSet = new HashSet<Integer>();
+        zipcodesSet.add(21337);
 
-        assertEquals(resultStream.collect(Collectors.toList()),
-                expectedStream.collect(Collectors.toList()));
+        Integer minIncome = 5;
+        Integer maxIncome = 15;
+
+        Map<Person.Gender, Set<Person>> resultMap;
+        Map<Person.Gender, Set<Person>> expMap = new HashMap<Person.Gender, Set<Person>>();
+
+        Set<Person> persons = new HashSet<Person>();
+        persons.add(person1);
+        persons.add(person2);
+
+        expMap.put(Gender.MALE, persons);
+
+        Stream<Person> personStream = Stream.of(person1, person2);
+
+        resultMap = StreamOperations.groupPersonsByGender(personStream, zipcodesSet, minIncome,
+                maxIncome);
+
+        assertEquals(resultMap, expMap);
     }
 }
